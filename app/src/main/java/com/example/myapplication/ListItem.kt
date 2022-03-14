c4620d0b4a782f893d43d46a9041b82da98b7255
mariPrikolota <88455793+mariPrikolota@users.noreply.github.com>
2022-03-14 15:34:14 +0300

    refact

--- app/src/main/java/com/example/myapplication/ListItem.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication
-
-data class ListItem(
-    var quest: String
-)
--- app/src/main/java/com/example/myapplication/NavigationActivity.kt
+++ app/src/main/java/com/example/myapplication/NavigationActivity.kt
@@ -6,30 +6,12 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.navigation_layout.*
 
 class NavigationActivity : AppCompatActivity() {
-    private val homeFragment = HomeFragment()
-    private val gameFragment = GameFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_layout)
 
-        configureNavigationBar()
-    }
-
-    private fun configureNavigationBar() {
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, homeFragment).commit()
-
-        navigationView.setOnItemSelectedListener { item ->
-            var selectFragment : Fragment? = null
-
-            when (item.itemId) {
-                R.id.home -> selectFragment = homeFragment
-                R.id.game -> selectFragment = gameFragment
-            }
-
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, selectFragment!!).commit()
-            true
-        }
     }
 
 }
