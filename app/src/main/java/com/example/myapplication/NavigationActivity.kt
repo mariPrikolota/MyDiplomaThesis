522980fa68dcb5c030d5d1a25a792d5b3ebdc674
mariPrikolota <88455793+mariPrikolota@users.noreply.github.com>
2022-03-17 18:18:32 +0300

    ba

--- app/src/main/java/com/example/myapplication/NavigationActivity.kt
+++ app/src/main/java/com/example/myapplication/NavigationActivity.kt
@@ -2,15 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.navigation_layout.*
+
 
 class NavigationActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.navigation_layout)
+        setContentView(R.layout.home_layout)
 
     }
 
